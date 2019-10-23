ad9975f1865f8ed3209e026846951951b4893b74
Mindaugas Kuciauskas (MKUC) <mkuc@oticon.com>
2019-10-23 15:43:03 +0200

    starting to add collapse

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-import react, { useState } from 'react';
+import { useState } from 'react';
 import fetch from 'isomorphic-unfetch';
-import { Container, Row, Col } from 'react-bootstrap';
 
 const columns = ['Thumbnail', 'Title', 'Description', 'Players', 'Playtime', 'MinAge', 'Select'];
 
@@ -10,8 +9,8 @@ export default function index(props) {
 		<div>
 			<link
 				rel="stylesheet"
-				href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css"
-				integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4"
+				href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+				integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
 				crossorigin="anonymous"
 			/>
 			<table className="table table-dark">
@@ -32,7 +31,26 @@ export default function index(props) {
 									<img src={x.thumbnail} />
 								</td>
 								<td>{x.gameTitle}</td>
-								<td>{x.description}</td>
+								<td>
+									<div class="card">
+										<div class="card-header" id="b">
+											<button
+												class="btn btn-link"
+												type="button"
+												data-toggle="collapse"
+												data-target={'#' + x.gameTitle}
+												aria-expanded="true"
+												aria-controls={x.gameTitle}
+											>
+												Description
+											</button>
+										</div>
+
+										<div id={x.gameTitle} class="collapse show" aria-labelledby="b" data-parent="#accordionExample">
+											<div class="card-body">{x.description}</div>
+										</div>
+									</div>
+								</td>
 								<td>
 									{x.minplayers}-{x.maxplayers}
 								</td>
@@ -43,7 +61,7 @@ export default function index(props) {
 								<td>
 									<input
 										type="checkbox"
-										onChange={(e, d) => {
+										onChange={() => {
 											setSelectedGames(selectedGames.concat(x.gameTitle));
 											console.log(selectedGames);
 										}}
@@ -54,14 +72,28 @@ export default function index(props) {
 					})}
 				</tbody>
 			</table>
+			<script
+				src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+				integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+				crossorigin="anonymous"
+			></script>
+			<script
+				src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+				integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+				crossorigin="anonymous"
+			></script>
+			<script
+				src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+				integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+				crossorigin="anonymous"
+			></script>
 		</div>
 	);
 }
 
 index.getInitialProps = async function() {
-	const response = await fetch(
-		'https://bgg-api.azurewebsites.net/api/GetWishlistDetailedLocalTable?code=4GcBaUuR/mQWefy9Lu9DBN2kLZ2Al2Ju4sasuwNho7aqWe3zchW5KQ=='
-	);
+	const response = await fetch('http://localhost:7071/api/GetWishlistDetailedLocalTable');
+	//const response = await fetch('https://bgg-api.azurewebsites.net/api/GetWishlistDetailedLocalTable?code=4GcBaUuR/mQWefy9Lu9DBN2kLZ2Al2Ju4sasuwNho7aqWe3zchW5KQ==');
 	const data = await response.json();
 	return { games: data.map(entry => entry) };
 };
